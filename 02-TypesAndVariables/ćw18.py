037895466726e46c6d0b7ae36f2aa72d4f7bab22
Mikołaj Suchan <225887@student.uek.krakow.pl>
2022-10-15 14:08:00 +0200

    poprawka

--- 02-TypesAndVariables/ćw18.py
+++ 02-TypesAndVariables/ćw18.py
@@ -2,6 +2,6 @@ import math
 a=int(input("Podaj długość pierwszego boku:\n"))
 b=int(input("Podaj długośc drugiego boku\n"))
 c=int(input("Podaj długość trzeciego boku\n"))
-p=int((a+b+c)/2)
-s=int(math.sqrt(p*(p-a)*(p-b)*(p-c)))
+p=(a+b+c)/2
+s=math.sqrt(p*(p-a)*(p-b)*(p-c)),
 print(f"Pole wynosi {s} jednostek kwadratowych")
